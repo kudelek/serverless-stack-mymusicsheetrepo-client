21c53f45f81f74fa3b3a935265c5163f6fd7eb43
Konrad Zapotoczny <kzapotoczny06@gmail.com>
2021-04-24 19:06:13 +0200

    Adding invalid method

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -35,9 +35,9 @@ export default function Home() {
     onLoad();
   }, [isAuthenticated]);
 
-   function loadMusicSheet() {
-     return API.get("mymusicsheetrepo-api", "/ASDASFSASADSADASDSADFS");
-   }
+  function loadNotes() {
+    return API.put("musicsheet", "/musicsheet");
+  }
 
   function renderMusicSheetList(musicsheet) {
     return (
